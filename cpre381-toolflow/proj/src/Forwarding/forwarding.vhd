0e5aa9f7065e7954554e30167be067165bcb5f13
jbarnes3 <jbarnes3@iastate.edu>
2024-11-20 14:31:53 -0600

    Finish Forwarding

--- cpre381-toolflow/proj/src/Forwarding/forwarding.vhd
+++ cpre381-toolflow/proj/src/Forwarding/forwarding.vhd
@@ -1,14 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL; 
 
 entity forwarding is
     port (
-        rs           : in std_logic_vector(4 downto 0);  -- register 1 in EX stage
-        rt           : in std_logic_vector(4 downto 0);  -- register 2 in EX stage
-        MEM_RD       : in std_logic_vector(4 downto 0);  -- register in MEM stage
-        WB_RD        : in std_logic_vector(4 downto 0);  -- register in WB stage
-        MEM_RegWrite : in std_logic;                     -- signal for MEM stage
-        WB_RegWrite  : in std_logic;                     -- signal for WB stage
+        rs           : in std_logic_vector(4 downto 0);  -- Register 1 in EX stage
+        rt           : in std_logic_vector(4 downto 0);  -- Register 2 in EX stage
+        MEM_RD       : in std_logic_vector(4 downto 0);  -- Register in MEM stage
+        WB_RD        : in std_logic_vector(4 downto 0);  -- Register in WB stage
+        MEM_RegWrite : in std_logic;                     -- Signal for MEM stage
+        WB_RegWrite  : in std_logic;                     -- Signal for WB stage
         forward_A    : out std_logic_vector(1 downto 0); -- Forwarding control for rs
         forward_B    : out std_logic_vector(1 downto 0)  -- Forwarding control for rt
     );
@@ -16,15 +17,23 @@ end forwarding;
 
 architecture Behavioral of forwarding is
 begin
-    -- Forwarding control for rs 
-    with (MEM_RegWrite & MEM_RD & rs) select
-        forward_A <= "10" when (MEM_RegWrite = '1' and MEM_RD = rs),
-                     "01" when (WB_RegWrite = '1' and WB_RD = rs),
-                     "00" when others;
+    process(rs, rt, MEM_RD, WB_RD, MEM_RegWrite, WB_RegWrite)
+    begin
+        forward_A <= "00";
+        forward_B <= "00";
+        -- Forwarding logic for rs
+        if MEM_RegWrite = '1' and MEM_RD /= "00000" and MEM_RD = rs then
+            forward_A <= "10"; 
+        elsif WB_RegWrite = '1' and WB_RD /= "00000" and WB_RD = rs then
+            forward_A <= "01";
+        end if;
 
-    -- Forwarding control for rt 
-    with (MEM_RegWrite & MEM_RD & rt) select
-        forward_B <= "10" when (MEM_RegWrite = '1' and MEM_RD = rt),
-                     "01" when (WB_RegWrite = '1' and WB_RD = rt),
-                     "00" when others;
+        -- Forwarding logic for rt
+        if MEM_RegWrite = '1' and MEM_RD /= "00000" and MEM_RD = rt then
+            forward_B <= "10";
+        elsif WB_RegWrite = '1' and WB_RD /= "00000" and WB_RD = rt then
+            forward_B <= "01";
+        end if;
+    end process;
 end Behavioral;
+
