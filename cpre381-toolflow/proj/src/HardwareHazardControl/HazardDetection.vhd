0bae1a53e9e03db0a581682043fb87d0adfa3b0b
jarceo <jarceo@iastate.edu>
2024-11-30 13:31:31 -0600

    Slowly adding to hazard detection

--- cpre381-toolflow/proj/src/HardwareHazardControl/HazardDetection.vhd
+++ cpre381-toolflow/proj/src/HardwareHazardControl/HazardDetection.vhd
@@ -5,6 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity HazardDetection is
     port (
         i_EX_branch     : std_logic;
+        
 
 
         o_IDIF_Flush     : std_logic;
@@ -18,4 +19,23 @@ entity HazardDetection is
     );
 end HazardDetection;
 
+architecture mixed of HazardDetection is
+    begin
 
+        o_IDIF_Flush  <= '0';
+        o_IFEX_Flush  <= '0';
+        o_EXMEM_Flush <= '0';
+        o_MEMWB_Flush <= '0';
+
+
+
+
+        -- Flush on branch
+        if (i_EX_branch) then
+            o_IDIF_Flush  <= '1';
+            o_IFEX_Flush  <= '1';
+            o_EXMEM_Flush <= '1';
+            o_MEMWB_Flush <= '1';
+        end if;
+
+    end mixed;
