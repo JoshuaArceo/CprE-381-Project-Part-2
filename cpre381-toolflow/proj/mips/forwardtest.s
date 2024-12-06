49aeba4fdc1cf35c2833debabf5757a80003303b
jarceo <jarceo@iastate.edu>
2024-12-06 03:43:32 -0600

    Wrapping up forwarding

--- cpre381-toolflow/proj/mips/forwardtest.s
+++ cpre381-toolflow/proj/mips/forwardtest.s
@@ -1,11 +1,13 @@
-.data
+
+
+    .data
 .text
 .globl main
 main:
     # Start Test
     lui $5, 0x1001
     addi $1, $0, 4   
-    add $2, $0, $1  
+    add $2, $1, $2  
     add $3, $1, $2  
     sw   $2, 0($5)
     addi $1, $0, 10 
@@ -16,24 +18,3 @@ main:
 
 
     halt
-
-
-
-#     .data
-# .text
-# .globl main
-# main:
-#     # Start Test
-#     lui $5, 0x1001
-#     addi $1, $0, 4   
-#     add $2, $1, $2  
-#     add $3, $1, $2  
-#     sw   $2, 0($5)
-#     addi $1, $0, 10 
-#     addi $2, $2, 12  
-#     addi $6, $3, 12  
-#     lw  $4, 0($5)
-#     sw  $4, 0($5)
-
-
-#     halt
